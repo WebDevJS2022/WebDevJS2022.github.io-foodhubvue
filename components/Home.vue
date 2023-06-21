0336f9e77c2a6954783d2227034342f7c000fb7c
WeDev <webdevjs2022@gmail.com>
2023-06-21 10:49:06 +0500

    Добавление верстки

---
+++ components/Home.vue
@@ -0,0 +1,353 @@
+<template>
+    <body>
+    
+    <header class="header">
+        <div class="container">
+            <div class="header__wrapper">
+    
+                <div class="header__logo">
+                    <a href="index.html" class="header__logo-link">
+                        <img class="header__logo-img" src="../images/src/headerlogo.png" alt="logo">
+                    </a>
+                </div>
+    
+                <nav class="menu">
+    
+                    <div class="menu__btn">
+                        <span></span>
+                    </div>
+    
+    
+                    <ul class="menu__list">
+                        <li class="menu__list-item">
+                            <a class="menu__list-link" href="#">Home</a>
+                        </li>
+                        <li class="menu__list-item">
+                            <a class="menu__list-link" href="menu.html">Menu</a>
+                        </li>
+                        <li class="menu__list-item">
+                            <a class="menu__list-link" href="#">Service</a>
+                        </li>
+                        <li class="menu__list-item">
+                            <a class="menu__list-link" href="#">Delivery</a>
+                        </li>
+                        <li class="menu__list-item">
+                            <a class="menu__list-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div class="header__buttons">
+                    <button class="header__login">Login</button>
+                    <a class="header__cart">
+                        <img src="../images/src/cart.png" alt="icon" class="header__cart-img">
+                        <span class="header__count" aria-label="Количество товаров">3</span>
+                    </a>
+                </div>
+    
+            </div>
+    
+        </div>
+    </header>
+
+    <div class="wrapper">
+        <main class="main">
+            <section class="top">
+                <div class="container">
+                    <div class="top__wrapper">
+                        <div class="top__content">
+                            <p class="top__content-subtitle">Welcome to Foodhub</p>
+                            <h1 class="top__content-title">Enjoy Healthy
+                                & Tasty Food</h1>
+                            <p class="top__content-text">Food is usually of plant, animal or fungal origin, and contains essential nutrients, such as carbohydrates, fats, proteins, vitamins, or minerals.</p>
+
+                            <button class="top__content-btn">Explore Menu</button>
+                        </div>
+                        <div class="top__images">
+                            <img src="../images/src/top1.png" alt="image" class="top__images-1">
+                            <img src="../images/src/top2.png" alt="image" class="top__images-2">
+                            <img src="../images/src/top3.png" alt="image" class="top__images-3">
+                            <img src="../images/src/top4.png" alt="image" class="top__images-4">
+                            <img src="../images/src/top5.png" alt="image" class="top__images-5">
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="service">
+                <div class="container">
+                    <div class="service__wrapper">
+                        <div class="service__item">
+                            <div class="service__image">
+                                <img src="../images/src/service1.png" alt="icon" class="service__img service__img-1">
+                            </div>
+                            <h3 class="service__title">Online Food Sell</h3>
+                        </div>
+                        <div class="service__item">
+                            <div class="service__image">
+                                <img src="../images/src/service2.png" alt="icon" class="service__img service__img-2">
+                            </div>
+                            <h3 class="service__title">Delivery Service</h3>
+                        </div>
+                        <div class="service__item">
+                            <div class="service__image">
+                                <img src="../images/src/service3.png" alt="icon" class="service__img service__img-3">
+                            </div>
+                            <h3 class="service__title">Catering Service</h3>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="about">
+                <div class="container">
+                    <div class="about__wrapper">
+                        <div class="about__image">
+                            <img src="../images/src/about.png" alt="image" class="about__img">
+                        </div>
+
+                        <div class="about__content">
+                            <h2 class="about__title">Serving Best Food is Our First Priority</h2>
+                            <p class="about__text">nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion</p>
+                            <button class="about__btn">Learn About Us</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="offer">
+                <div class="container">
+                    <h2 class="offer__title">We Offer People
+                        Many Type of Food</h2>
+                    <div class="offer__items">
+                        <div class="offer__item">
+                            <div class="offer__item-top">
+                                <div class="offer__image">
+                                    <img src="../images/src/offer1.png" alt="image" class="offer__img">
+                                </div>
+                                <div class="offer__item-stars">
+                                    <img src="../images/src/stars.png" alt="icon" class="offer__stars">
+                                    <p class="offer__item-avaible">300+ Items Availble</p>
+                                </div>
+                            </div>
+            
+                            <div class="offer__item-down">
+                                <h3 class="offer__item-title">Local/Traditional Food</h3>
+                                <p class="offer__item-text">It is a long established fact that a reader will be distracted by the
+                                    readable content of a page when looking at its layout.</p>
+                            </div>
+                        </div>
+                        <div class="offer__item">
+                            <div class="offer__item-top">
+                                <div class="offer__image">
+                                    <img src="../images/src/offer2.png" alt="image" class="offer__img">
+                                </div>
+                                <div class="offer__item-stars">
+                                    <img src="../images/src/stars.png" alt="icon" class="offer__stars">
+                                    <p class="offer__item-avaible">140+ Items Availble</p>
+                                </div>
+                            </div>
+            
+                            <div class="offer__item-down">
+                                <h3 class="offer__item-title">Inter/Continental Food</h3>
+                                <p class="offer__item-text">It is a long established fact that a reader will be distracted by the
+                                    readable content of a page when looking at its layout.</p>
+                            </div>
+                        </div>
+                        <div class="offer__item">
+                            <div class="offer__item-top">
+                                <div class="offer__image">
+                                    <img src="../images/src/offer3.png" alt="image" class="offer__img">
+                                </div>
+                                <div class="offer__item-stars">
+                                    <img src="../images/src/stars.png" alt="icon" class="offer__stars">
+                                    <p class="offer__item-avaible">200+ Items Availble</p>
+                                </div>
+                            </div>
+            
+                            <div class="offer__item-down">
+                                <h3 class="offer__item-title">Fastfood</h3>
+                                <p class="offer__item-text">It is a long established fact that a reader will be distracted by the
+                                    readable content of a page when looking at its layout.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="discount">
+                <div class="container">
+                    <h2 class="discount__title">Up to <span>50%</span> Discount on Selected Items</h2>
+                    <p class="discount__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    <div class="discount__buttons">
+                        <a href="#" class="discount__items-btn">All Discount Item</a>
+                        <a href="#" class="discount__menu-btn">View All Menu</a>
+                    </div>
+                </div>
+            </section>
+
+            <section class="dishes">
+                <div class="container">
+                    <h2 class="dishes__title">Our Most Popular Dishes</h2>
+                    <div class="dishes__list">
+                        <ul class="dishes__list-items">
+                            <li class="dishes__list-item">Breakfast</li>
+                            <li class="dishes__list-item">Lunch</li>
+                            <li class="dishes__list-item">Dinner</li>
+                            <li class="dishes__list-item">Fastfood</li>
+                            <li class="dishes__list-item">Drinks</li>
+                            <li class="dishes__list-item">Others</li>
+                        </ul>
+                    </div>
+
+                    <div class="dishes__items">
+                        <div class="dishes__item">
+                            <div class="dishes__price"><b>$14.53</b></div>
+                            <a href="item.html" class="dishes__link">
+                                <img src="../images/src/dish1.png" alt="image" class="dishes__item-img">
+                            </a>
+                    
+                            <div class="dishes__item-content">
+                                <div class="dishes__item-content-top">
+                                    <h3 class="dishes__item-content-title">Italian Fusilli Pasta</h3>
+                    
+                                    <div class="dishes__form-counter">
+                                        <button class="dishes__minus-btn" type="button" aria-label="Убрать один товар">-</button>
+                    
+                                        <input class="dishes__input" type="text" value="+2" name="count">
+                    
+                                        <button class="dishes__plus-btn" type="button" aria-label="Добавить один товар">+</button>
+                                    </div>
+                                </div>
+                    
+                                <div class="dishes__item-content-down">
+                                    <div class="dishes__content-time">25 Min Away</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="dishes__item">
+                            <div class="dishes__price"><b>$13.20</b></div>
+                            <a href="item.html" class="dishes__link">
+                                <img src="../images/src/dish2.png" alt="image" class="dishes__item-img">
+                            </a>
+                    
+                            <div class="dishes__item-content">
+                                <div class="dishes__item-content-top">
+                                    <h3 class="dishes__item-content-title">Braised belly caramelised </h3>
+                    
+                                    <div class="dishes__form-counter">
+                                        <button class="dishes__minus-btn" type="button" aria-label="Убрать один товар">-</button>
+                    
+                                        <input class="dishes__input" type="text" value="+1" name="count">
+                    
+                                        <button class="dishes__plus-btn" type="button" aria-label="Добавить один товар">+</button>
+                                    </div>
+                                </div>
+                    
+                                <div class="dishes__item-content-down">
+                                    <div class="dishes__content-time">45 Min Away</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="dishes__item">
+                            <div class="dishes__price"><b>$6.50</b></div>
+                            <a href="item.html" class="dishes__link">
+                                <img src="../images/src/dish3.png" alt="image" class="dishes__item-img">
+                            </a>
+                    
+                            <div class="dishes__item-content">
+                                <div class="dishes__item-content-top">
+                                    <h3 class="dishes__item-content-title">Fried Crap Bow Rice</h3>
+                    
+                                    <div class="dishes__form-counter">
+                                        <button class="dishes__minus-btn" type="button" aria-label="Убрать один товар">-</button>
+                    
+                                        <input class="dishes__input" type="text" value="+1" name="count">
+                    
+                                        <button class="dishes__plus-btn" type="button" aria-label="Добавить один товар">+</button>
+                                    </div>
+                                </div>
+                    
+                                <div class="dishes__item-content-down">
+                                    <div class="dishes__content-time">1 Hour Away</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="app">
+                <div class="container">
+                    <div class="app__wrapper">
+                        <div class="app__content">
+                            <h3 class="app__content-title">Easiest way to get your food by our app</h3>
+                            <button class="app__content-btn">Download now</button>
+                        </div>
+                    
+                        <img src="../images/src/app.png" alt="image" class="app__image">
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__wrapper">
+                <div class="footer__social">
+                    <img src="../images/src/headerlogo.png" alt="logo" class="footer__social-logo">
+                    <p class="footer__social-text">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those.</p>
+                </div>
+                <div class="footer__menu">
+                    <a href="menu.html" class="footer__menu-link">
+                        <h4 class="footer__title">Our Menu</h4>
+                    </a>
+                    <div class="footer__menu-links">
+                        <ul class="footer__links-list">
+                            <li class="footer__link-li"><a href="#" class="footer__link">Breakfast</a></li>
+                            <li class="footer__link-li"><a href="#" class="footer__link">Lunch</a></li>
+                            <li class="footer__link-li"><a href="#" class="footer__link">Dinner</a></li>
+                            <li class="footer__link-li"><a href="#" class="footer__link">Snacks</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer__links">
+                    <h4 class="footer__title">UsefulLinks</h4>
+                    <div class="footer__links-links">
+                        <ul class="footer__links-list">
+                            <li class="footer__link-li"><a href="#" class="footer__link">Terms & Conditions</a></li>
+                            <li class="footer__link-li"><a href="#" class="footer__link">Services</a></li>
+                            <li class="footer__link-li"><a href="#" class="footer__link">Payment & Tax</a></li>
+                            <li class="footer__link-li"><a href="#" class="footer__link">Support</a></li>
+                            <li class="footer__link-li"><a href="#" class="footer__link">Return Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer__info">
+                    <h4 class="footer__title">Information</h4>
+                    <a href="#" class="footer__link footer__link-info">Get Update</a>
+                    <form action=" " class="footer__form">
+                        <input type="email" class="footer__input" placeholder="Email Address...">
+                        <input type="submit" value=" " class="footer__button" style="background-image: url(../images/src/tg.png); background-repeat: no-repeat; background-position: center;">
+                    </form>
+                    <a href="#" class="footer__link">Payment Method</a>
+                    <img src="../images/src/payment.png" alt="payment" class="footer__payment">
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="js/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+    </body>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
